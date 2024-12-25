e3ca501b6eb1b18897f14a6726770d885cd5262e
fengshgx <33620209+fengshgx@users.noreply.github.com>
2024-12-25 12:10:54 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '22037cab-b239-480e-9e8f-8827b7f6aacf';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
